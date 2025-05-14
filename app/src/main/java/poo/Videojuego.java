1541afe8452be33ae5f0538ee5a38a9413ebe31a
Fedeguez18 <fedese18@gmail.com>
2025-05-14 16:56:38 -0300

    clase videojuego

---
+++ app/src/main/java/poo/Videojuego.java
@@ -0,0 +1,38 @@
+package poo;
+
+import java.awt.Graphics2D;
+
+import com.entropyinteractive.JGame;
+
+public class Videojuego extends JGame {
+
+    public Videojuego(String arg0, int arg1, int arg2) {
+        super(arg0, arg1, arg2);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void gameDraw(Graphics2D arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'gameDraw'");
+    }
+
+    @Override
+    public void gameShutdown() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'gameShutdown'");
+    }
+
+    @Override
+    public void gameStartup() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'gameStartup'");
+    }
+
+    @Override
+    public void gameUpdate(double arg0) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'gameUpdate'");
+    }
+    
+}
