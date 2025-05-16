42e2dc07fef6f5adc357d79ae6f6e99df618014d
Tommy <tomasfwat@gmail.com>
2025-05-16 19:29:05 -0300

    Se agregaron nuevas clases:Juego,Pong y PantallaInicioPong. Se paso toda la info anterior de JuegoPong a Pong, y se modifico todo JuegoPong

---
+++ app/src/main/java/poo/Juego.java
@@ -0,0 +1,51 @@
+package poo;
+
+public abstract class Juego {
+    private String nombre;
+    private String desarrolladores;
+    private String version;
+    private String descripcion;
+    private boolean implementado;
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setDesarrolladores(String desarrolladores){
+        this.desarrolladores=desarrolladores;
+    }
+
+    public String getDesarrolladores(){
+        return this.desarrolladores;
+    }
+
+    public void setVersion(String version){
+        this.version=version;
+    }
+
+    public String getVersion(){
+        return this.version;
+    }
+
+    public void setDescripcion(String descripcion){
+        this.descripcion=descripcion;
+    }
+
+    public String getDescripcion(){
+        return this.descripcion;
+    }
+
+    public void setImplementado(boolean implementado){
+        this.implementado=implementado;
+    }
+
+    public boolean isImplementado(){
+        return this.implementado;
+    }
+
+    public abstract void run();
+}
