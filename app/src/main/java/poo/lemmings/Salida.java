f3caa2af69fa0263b6b155b9889167b27818c1e9
Tommy <tomasfwat@gmail.com>
2025-05-26 17:19:45 -0300

    Cambios en Entrada y agrgeo la imagen de la salida en la clase Salida

--- app/src/main/java/poo/lemmings/Salida.java
+++ app/src/main/java/poo/lemmings/Salida.java
@@ -1,19 +1,29 @@
 package poo.lemmings;
 
-import org.example.ObjetoGrafico;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import org.example.ObjetoGrafico;
 
 public class Salida extends ObjetoGrafico {
 
     private int contadorRecibidos;
+    protected BufferedImage spriteSheet; 
+    protected BufferedImage spritePuertaSalida;
 
-
-    public Salida(String filename) {
-        super(filename);
-        
+    public Salida() {
+        super("imagenes/Lemmings_Puertas_Decoraciones",010,200);
+        try {
+            spriteSheet = ImageIO.read(getClass().getClassLoader().getResourceAsStream("imagenes/Lemmings_Puertas_Decoraciones"));
+            spritePuertaSalida = spriteSheet.getSubimage(1218, 0, 60, 52);
+        } catch (IOException e){
+            throw new RuntimeException("Error al cargar la imagen :(",e);
+        }
     }
 
-
+    //Con este metodo cuento los lemmings que se salvaron
     public void salvarLemming(Lemming lemming) {
        //removerLemming(lemming); 
         contadorRecibidos++;
